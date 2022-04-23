18f7351e7991dc2bd5db018c78c698e5993aade6
Ilya <mac@MacBookPro.local>
2022-04-23 15:34:05 +0300

    add get timeout news and kurs

--- backend/server.js
+++ backend/server.js
@@ -19,31 +19,39 @@ bot.setWebHook(`${config.get('url')}/bot${config.get('token')}`);
   bot.sendMessage(id, `clone ${msg.text}`);
 });*/
 
-bot.onText(/\/kurs/, (msg, [ source, match ]) => {
-  const { chat: { id } } = msg;
+// GET News
+let counter = 0;
+const chatID = 396087497;
+const timeNews = (1000 * 60) * 60; // Каждый час
+const timeKURS = ((1000 * 60) * 60) * 6; // Каждые 6 часов
+axios
+  .get(`https://newsapi.org/v2/everything?q=Apple&from=2022-04-23&sortBy=popularity&apiKey=${config.get('tokenNews')}`)
+  .then(async (res) => {
+    setInterval(async () => {
+      if (res.data.articles.length === counter) {
+        counter = 0;
+      } else {
+        counter ++;
+      }
 
-  axios
-    .get(`https://currate.ru/api/?get=rates&pairs=USDRUB,EURRUB,BTCUSD,ETHEUR,ETHRUB,BTCEUR,BTCRUB,ETHUSD&key=${config.get('tokenCurrate')}`)
-    .then(async (res) => {
+      const parsedHtml = await parsers.news(res.data.articles[counter]);
+      bot.sendMessage(chatID, parsedHtml);
+    }, timeNews);
+  })
+  .catch(err => console.log(err));
+
+// GET KURS
+axios
+  .get(`https://currate.ru/api/?get=rates&pairs=USDRUB,EURRUB,BTCUSD,ETHEUR,ETHRUB,BTCEUR,BTCRUB,ETHUSD&key=${config.get('tokenCurrate')}`)
+  .then(async (res) => {
+    setInterval(async () => {
       const parsedHtml = await parsers.kurs(res.data.data);
       await bot.sendMessage(id, parsedHtml);
-    })
-    .catch(err => console.log(err));
-});
+    }, timeKURS);
+  })
+  .catch(err => console.log(err));
 
-bot.onText(/\/news/, (msg, [ source, match ]) => {
-  const { chat: { id } } = msg;
-
-  axios
-    .get(`https://newsapi.org/v2/everything?q=tesla&from=2022-03-22&sortBy=publishedAt&apiKey=${config.get('tokenNews')}`)
-    .then(async (res) => {
-      const parsedHtml = await parsers.news(res.data.articles.slice(-1)[0]);
-      bot.sendMessage(id, parsedHtml);
-    })
-    .catch(err => console.log(err));
-});
-
-bot.onText(/\/gotoviy_biznes/, (msg, [ source, match ]) => {
+/*bot.onText(/\/gotoviy_biznes/, (msg, [ source, match ]) => {
   const { chat: { id } } = msg;
   axios
     .get('https://xkcd.com/info.0.json')
@@ -55,40 +63,24 @@ bot.onText(/\/gotoviy_biznes/, (msg, [ source, match ]) => {
         reply_markup: JSON.stringify({
           inline_keyboard: [
             [{ text: 'Кнопка 1', callback_data: '1' }],
-            [{
-              text: 'Google',
-              url: 'https://google.com'
-            }],
+            [{ text: 'Google', url: 'https://google.com' }],
             [
-              {
-                text: 'Back to the chat',
-                switch_inline_query: 'Hello again'
-              },
-              {
-                text: 'Stay here and talk to me again',
-                switch_inline_query_current_chat: 'It`s love'
-              }
+              { text: 'Back to the chat', switch_inline_query: 'Hello again' },
+              { text: 'Stay here and talk to me again', switch_inline_query_current_chat: 'It`s love' }
             ],
             [{ text: 'Кнопка 2', callback_data: 'data 2' }],
             [{ text: 'Кнопка 3', callback_data: 'text 3' }],
-            [{
-              text: 'Yandex',
-              url: 'https://yandex.ru'
-            }],
-            [
-              {
-                text: 'Show alert message',
-                callback_data: 'Hello world!!'
-              }
-            ]
+            [{ text: 'Yandex', url: 'https://yandex.ru' }],
+            [{ text: 'Show alert message', callback_data: 'Hello world!!' }]
           ]
         })
       });
     })
     .catch(err => console.log(err));
-});
+});*/
 
+/*
 bot.onText(/\/cars/, (msg, [ source, match ]) => {
   const { chat: { id } } = msg;
   bot.sendMessage(id, 'Hello cars');
-});
+});*/
